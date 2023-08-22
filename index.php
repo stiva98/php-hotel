9d64e7dcc50b709d8a59b4e2ad83baeb6d3f11d6
stiva98 <samustiva9810@gmail.com>
2023-08-22 22:58:28 +0200

    ADD bonus 1

--- index.php
+++ index.php
@@ -40,6 +40,8 @@
 
     ];
 
+    $filterParking=$_GET['filterParking'] ?? 'off';
+
 ?>
 
 <!DOCTYPE html>
@@ -57,45 +59,49 @@
         </h1>
     </header>
     <main>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nome Hotel</th>
-      <th scope="col">Descrizione</th>
-      <th scope="col">Parcheggio</th>
-      <th scope="col">Voto</th>
-      <th scope="col">Distanza dal centro</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-        foreach ($hotels as $index => $hotel) {
-    ?>
-        <tr>
-        <th scope="row"><?php echo $index; ?></th>
-        <td><?php echo $hotel['name']; ?></td>
-        <td><?php echo $hotel['description']; ?></td>
-        <td><?php echo $hotel['parking']; ?></td>
-        <td><?php echo $hotel['vote']; ?></td>
-        <td><?php echo $hotel['distance_to_center']; ?> km</td>
-        </tr>
-    <?php
-        }
-    ?>
-    <!-- <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr> -->
-  </tbody>
-</table>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome Hotel</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Parcheggio</th>
+                    <th scope="col">Voto</th>
+                    <th scope="col">Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    foreach ($hotels as $index => $hotel) {
+                        if ($filterParking == 'off' || $hotel['parking']) {    
+                ?>
+                    <tr>
+                        <th scope="row"><?php echo $index; ?></th>
+                        <td><?php echo $hotel['name']; ?></td>
+                        <td><?php echo $hotel['description']; ?></td>
+                        <td><?php if ($hotel['parking'] == true) {
+                            echo 'SI';
+                        } else {
+                            echo 'NO';
+                        }  ?></td>
+                        <td><?php echo $hotel['vote']; ?></td>
+                        <td><?php echo $hotel['distance_to_center']; ?> km</td>
+                    </tr>
+                <?php
+                    }
+                    }
+                ?>
+            </tbody>
+        </table>
+        <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+            <form action="index.php" method="get">
+                <input type="checkbox" class="btn-check" id="btncheck1" autocomplete="off" name="filterParking" <?php if ($filterParking == 'on') {
+                    echo 'checked';
+                } ?>>
+                <label class="btn btn-outline-primary" for="btncheck1">Parcheggio</label>
+                <button type="submit" class="btn btn-outline-primary">INVIA</button>
+            </form>
+        </div>
     </main>
 </body>
 </html>
